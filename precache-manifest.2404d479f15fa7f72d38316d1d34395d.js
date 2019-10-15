6aacf3162b2388a51d3377abfb1046bc966cb83c
Noah Tanner <ntanner@student.neumont.edu>
2019-10-15 16:55:22 -0600

    Updates

---
+++ precache-manifest.2404d479f15fa7f72d38316d1d34395d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd573a1e263349b49a834a6940beba3",
+    "url": "/ShineShare/static/media/ShineShareTemp.9dd573a1.png"
+  },
+  {
+    "revision": "b2a3a6232fedbe765dea",
+    "url": "/ShineShare/static/js/runtime~main.2d0abac2.js"
+  },
+  {
+    "revision": "5f468afbc7006c27d447",
+    "url": "/ShineShare/static/js/main.18a04e7b.chunk.js"
+  },
+  {
+    "revision": "4d7e96be59193d519db4",
+    "url": "/ShineShare/static/js/2.217bb704.chunk.js"
+  },
+  {
+    "revision": "5f468afbc7006c27d447",
+    "url": "/ShineShare/static/css/main.95d8e059.chunk.css"
+  },
+  {
+    "revision": "aec752c292c6013f49a283365c0d2e65",
+    "url": "/ShineShare/index.html"
+  }
+];
